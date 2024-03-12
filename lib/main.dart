49b2d1fcb456325f6513140e757d6d593840a360
noyevate <samuelnoye35@gmail.com>
2024-03-11 20:34:25 -0700

    more on folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,44 @@
+import 'package:chopnow/common/color_extension.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+  Widget defaltHome = const MyHomePage(title: "Chopnow");
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// void main() => runApp(MyApp());
 
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chopnow',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'ChopNow home page'),
+    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
+    return ScreenUtilInit(
+      designSize: const Size(720, 1640),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (context , child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Chopnow',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+            scaffoldBackgroundColor: Tcolor.primary,
+            iconTheme: IconThemeData(color: Color.fromRGBO(12, 21, 12, 0.5)),
+            primarySwatch: Colors.blueGrey,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: defaltHome,
+        );
+      },
+      //child: const MyHomePage(title: 'Chopnow'),
     );
   }
 }
