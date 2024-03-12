c1ffcedfe26e25c76c31df6fd48b5877166b275f
noyevate <samuelnoye35@gmail.com>
2024-03-11 20:09:51 -0700

    folder structure

---
+++ lib/common/color_extension.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Tcolor{
+  static Color get primary => const Color(0xffD7AB3B);
+  static Color get primaryText => const Color(0xffDBCDB2);
+  static Color get secondaryText => const Color(0xff000000);
+  //static Color get textField => const Color();
+  static Color get placeHolder => const Color(0xff9C9891);
+  static Color get white => const Color(0xffffffff);
+}
